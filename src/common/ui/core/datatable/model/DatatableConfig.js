e38a97f0ff8b410603a4caff52396aa515c8ed8d
Marcio Robson da Silva da Penha <robsonmrsp@gmail.com>
2020-10-08 21:04:37 -0300

    using actualPage instead current page on DatatableConfig

--- src/common/ui/core/datatable/model/DatatableConfig.js
+++ src/common/ui/core/datatable/model/DatatableConfig.js
@@ -1,13 +1,13 @@
 
 export default class DatatableConfig {
-  constructor({ currentPage = 1, pageSize = 10, items = [], totalRecords = 0, loading = false }) {
+  constructor({ actualPage = 1, pageSize = 10, items = [], totalRecords = 0, loading = false }) {
     this.pageSize = pageSize;
-    this.currentPage = currentPage;
+    this.actualPage = actualPage;
     this.totalRecords = totalRecords;
     this.items = items;
     this.loading = loading;
-    this.first = ((this.currentPage - 1) * this.pageSize) + 1;
-    this.last = Math.min(this.currentPage * this.pageSize, this.totalRecords);
+    this.first = ((this.actualPage - 1) * this.pageSize) + 1;
+    this.last = Math.min(this.actualPage * this.pageSize, this.totalRecords);
     this.totalPages = Math.trunc((this.totalRecords / this.pageSize) + 0);
   }
 }
