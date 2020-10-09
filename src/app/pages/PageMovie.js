14d3cc38b8df494daedc6ad5a33c1e75dbf8250b
Marcio Robson da Silva da Penha <robsonmrsp@gmail.com>
2020-10-08 21:05:19 -0300

    add search form to pageMovies

--- src/app/pages/PageMovie.js
+++ src/app/pages/PageMovie.js
@@ -1,21 +1,24 @@
+
 import React, { useEffect } from 'react';
-
-import { Col, Row } from 'react-bootstrap';
-import { useHistory } from 'react-router-dom';
+import { Form, Col, Row, Button } from 'react-bootstrap';
+import { Field, Formik } from 'formik';
 import { useQuery } from 'react-query';
-
-import DatatableConfig from '@/common/ui/core/datatable/model/DatatableConfig';
+import { useHistory } from 'react-router-dom';
+import HttpRequest from '@/app/services/HttpRequest';
 import { Breadcrumb } from '@/common/ui/container/breadcrumb/Breadcrumb';
 import { Page } from '@/common/ui/container/page';
 import { DataTable } from '@/common/ui/core/datatable';
-import HttpRequest from '@/app/services/HttpRequest';
+import DatatableConfig from '@/common/ui/core/datatable/model/DatatableConfig';
+import { DateInput } from '@/common/ui/core/components/DateInput';
+import { NumberInput } from '@/common/ui/core/components/NumberInput';
 
 const api = new HttpRequest('rs/crud/movies');
-
+// we need to use the memo to avoid unecessary calls to APIand rerender
 const PageMovie = () => {
   // criar um hook proprio para encapsular TODOs esses que manipulam informacao de consulta e paginacao.
   const history = useHistory();
   const [parameters, setParameters] = React.useState({ page: 1 });
+  const [searchParameters, setSearchParameters] = React.useState({ page: 1 });
   const [tableConfig, setTableConfig] = React.useState({});
 
   const { data } = useQuery([parameters], api.getPage);
@@ -24,12 +27,8 @@ const PageMovie = () => {
   const onClickRemove = (item) => { console.log(item); };
 
   useEffect(() => {
-    console.log();
-    // Fazer o Pager que é devolvido pelo backend devolver tambem as informacoes para reconstruir o pagination no cliente
-    // devolver o pageZise, order, direction, etc...
-
     setTableConfig(new DatatableConfig({ ...data }));
-  }, [data]);
+  }, [data, searchParameters]);
 
   const columns = [
     {
@@ -45,17 +44,28 @@ const PageMovie = () => {
       head: 'Actions',
       cellRender: (item) => (
         <DataTable.SimpleActionsCell
+          key={`Actions_${JSON.stringify(item)}`}
           onClickRemove={() => onClickRemove(item)}
           onClickEdit={() => onClickEdit(item)}
+          item={item}
         />
       ),
     },
   ];
 
-  const paginate = (params) => {
+
+  const paginate = (params, reset) => {
     // tem que aproveitar o valor do anterior para poder combinar ordenacao, pagina e demais paramentros
-    const newParans = { ...parameters, ...params };
-    setParameters(newParans);
+    if (reset) {
+      setParameters(params);
+    } else {
+      const newParans = { ...parameters, ...params };
+      setParameters(newParans);
+    }
+  };
+
+  const onSearch = (movieParameter) => {
+    paginate({ ...movieParameter }, true);
   };
 
   return (
@@ -63,6 +73,60 @@ const PageMovie = () => {
       <Breadcrumb />
       <Page>
         <Page.Title text="Page Movies" />
+        <Page.SectionWrapper>
+          <Formik
+            initialValues={{}}
+            onSubmit={(values, actions) => onSearch(values, actions)}
+          >
+            {({ setFieldValue, handleChange, handleSubmit }) => (
+              <Form onSubmit={handleSubmit}>
+                <Row>
+                  <Form.Group as={Col} md="6">
+                    <Form.Label>Title</Form.Label>
+                    <Form.Control
+                      name="title"
+                      type="text"
+                      placeholder="Title"
+                      onChange={handleChange}
+                    />
+                  </Form.Group>
+
+                  <Form.Group as={Col} md="6">
+                    <Form.Label>Release Date</Form.Label>
+                    <Field
+                      name="releaseDate"
+                      as={DateInput}
+                      onChange={(value) => setFieldValue('releaseDate', value)}
+                    />
+                    <Form.Control.Feedback>Looks good!</Form.Control.Feedback>
+                  </Form.Group>
+                  <Form.Group as={Col} md="6">
+                    <Form.Label>Duration</Form.Label>
+                    <Field
+                      name="duration"
+                      as={NumberInput}
+                      onChange={(value) => setFieldValue('duration', value)}
+                    />
+                    <Form.Control.Feedback>Looks good!</Form.Control.Feedback>
+                  </Form.Group>
+                  <Form.Group as={Col} md="6">
+                    <Form.Label>Cost</Form.Label>
+                    <Field
+                      decimal
+                      name="cost"
+                      as={NumberInput}
+                      onChange={(value) => setFieldValue('cost', value)}
+                    />
+                    <Form.Control.Feedback>Looks good!</Form.Control.Feedback>
+                  </Form.Group>
+                </Row>
+                <Button className="btn btn-primary" type="submit">
+                  Search
+                </Button>
+              </Form>
+            )}
+          </Formik>
+        </Page.SectionWrapper>
         <Row>
           <Col xl={12}>
             <Page.SectionWrapper>
@@ -94,7 +158,7 @@ const PageMovie = () => {
                 <Col sm="12" md="7">
                   <DataTable.Pagination
                     onPaginate={(pageNumber) => paginate({ page: pageNumber })}
-                    page={tableConfig.currentPage}
+                    page={tableConfig.actualPage}
                     pageSize={tableConfig.pageSize}
                     totalItems={tableConfig.totalRecords}
                   />
